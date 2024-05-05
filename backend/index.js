5388855a7c87792605cd597278eecfa5ad55cdf7
RadwanTbeileh <tbeileh.radwan@gmail.com>
2024-05-05 15:41:49 +0300

    Register function and router

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 const cors = require("cors");
 require("./models/db");
 
-
-
+const usersRouter = require("./routes/users");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,9 +11,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Handles any other endpoints [unassigned - endpoints]
 
 
+app.use("/users", usersRouter);
 
 
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
