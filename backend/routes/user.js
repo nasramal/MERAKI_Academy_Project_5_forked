fe2553a47d10b63944b3884d1052bb811b08f833
nasramal <amalradwan.nasr@gmail.com>
2024-05-05 20:28:14 +0300

    login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { register,login } = require("../controllers/users");
+const {register,login } = require("../controllers/users");
 
 const usersRouter = express.Router();
 
