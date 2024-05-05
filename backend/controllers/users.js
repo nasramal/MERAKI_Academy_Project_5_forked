19d972dcc78f58e8c0218480a187e29fa61eee99
nasramal <amalradwan.nasr@gmail.com>
2024-05-05 20:21:55 +0300

    login

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,5 @@
 const pool = require("../models/db");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const saltRounds = parseInt(process.env.SALT);
 
@@ -30,7 +31,56 @@ const register = async (req, res) => {
       });
     });
 };
+const login = (req, res) => {
+  const password = req.body.password;
+  const email = req.body.email;
+  const query = `SELECT * FROM users WHERE email = $1`;
+  const data = [email.toLowerCase()];
+  pool
+    .query(query, data)
+    .then((result) => {
+      if (result.rows.length) {
+        bcrypt.compare(password, result.rows[0].password, (err, response) => {
+          if (err) res.json(err);
+          if (response) {
+            const payload = {
+              userId: result.rows[0].id,
+              firstName: result.rows[0].firstName,
+              role: result.rows[0].role_id,
+            };
+            const options = { expiresIn: "60m" };
+            const secret = process.env.SECRET;
+            const token = jwt.sign(payload, secret, options);
+            if (token) {
+              return res.status(200).json({
+                token,
+                success: true,
+                message: `Valid login credentials`,
+                userId:result.rows[0].id
+              });
+            } else {
+              throw Error;
+            }
+          } else {
+            res.status(403).json({
+              success: false,
+              message: `The email doesn’t exist or the password you’ve entered is incorrect`,
+            });
+          }
+        });
+      } else throw Error;
+    })
+    .catch((err) => {
+      res.status(403).json({
+        success: false,
+        message:
+          "The email doesn’t exist or the password you’ve entered is incorrect",
+        err,
+      });
+    });
+};
+
 
 module.exports = {
-  register,
+  register,login
 };
