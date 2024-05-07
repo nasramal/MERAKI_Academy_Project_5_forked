25a099e5aee253b08d4a544c3f722cc4380eb10a
RadwanTbeileh <tbeileh.radwan@gmail.com>
2024-05-07 22:28:39 +0300

    test1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,21 +1,21 @@
 const pool = require("../models/db");
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const saltRounds = parseInt(process.env.SALT);
 
 const register = async (req, res) => {
-  const { firstName, lastName, age, email, password } = req.body;
-  
+  const { firstName, lastName, age, email, password,role_id, phone } = req.body;
+ 
   const encryptedPassword = await bcrypt.hash(password, saltRounds);
 
   pool
-    .query(`INSERT INTO users (firstName, lastName, age, email, password, role_id) VALUES ($1,$2,$3,$4,$5,$6)`,[
+    .query(`INSERT INTO users (firstName, lastName, age, email, password, role_id , phone) VALUES ($1,$2,$3,$4,$5,$6,$7)`,[
       firstName,
       lastName,
       age,
       email.toLowerCase(),
       encryptedPassword,
-      "1",
+      role_id,
+      phone
     ])
     .then((result) => {
       res.status(200).json({
@@ -31,56 +31,7 @@ const register = async (req, res) => {
       });
     });
 };
-const login = (req, res) => {
-  const password = req.body.password;
-  const email = req.body.email;
-  const query = `SELECT * FROM users WHERE email = $1`;
-  const data = [email.toLowerCase()];
-  pool
-    .query(query, data)
-    .then((result) => {
-      if (result.rows.length) {
-        bcrypt.compare(password, result.rows[0].password, (err, response) => {
-          if (err) res.json(err);
-          if (response) {
-            const payload = {
-              userId: result.rows[0].id,
-              firstName: result.rows[0].firstName,
-              role: result.rows[0].role_id,
-            };
-            const options = { expiresIn: "60m" };
-            const secret = process.env.SECRET;
-            const token = jwt.sign(payload, secret, options);
-            if (token) {
-              return res.status(200).json({
-                token,
-                success: true,
-                message: `Valid login credentials`,
-                userId:result.rows[0].id
-              });
-            } else {
-              throw Error;
-            }
-          } else {
-            res.status(403).json({
-              success: false,
-              message: `The email doesn’t exist or the password you’ve entered is incorrect`,
-            });
-          }
-        });
-      } else throw Error;
-    })
-    .catch((err) => {
-      res.status(403).json({
-        success: false,
-        message:
-          "The email doesn’t exist or the password you’ve entered is incorrect",
-        err,
-      });
-    });
-};
-
 
 module.exports = {
-  register,login
+  register,
 };
