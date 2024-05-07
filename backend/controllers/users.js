9cdd0796880e888495383e42ff7c373effcd33f3
RadwanTbeileh <tbeileh.radwan@gmail.com>
2024-05-07 22:50:25 +0300

    marg

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,18 +3,24 @@ const bcrypt = require("bcryptjs");
 const saltRounds = parseInt(process.env.SALT);
 
 const register = async (req, res) => {
+
   const { firstName, lastName, age, email, password,role_id, phone } = req.body;
  
   const encryptedPassword = await bcrypt.hash(password, saltRounds);
 
   pool
     .query(`INSERT INTO users (firstName, lastName, age, email, password, role_id , phone) VALUES ($1,$2,$3,$4,$5,$6,$7)`,[
+
+  
+
       firstName,
       lastName,
       age,
       email.toLowerCase(),
       encryptedPassword,
+
       role_id,
+
       phone
     ])
     .then((result) => {
