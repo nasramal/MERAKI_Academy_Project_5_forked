7599f07a7027a2612c49fe7d9154cdd2e070324c
FarahRefaie <farahrefaie.fr@gmail.com>
2024-05-07 21:57:11 +0300

    sql edited

--- backend/models/schema.sql
+++ backend/models/schema.sql
@@ -1,113 +1,124 @@
+
 CREATE TABLE roles (
-  id SERIAL NOT NULL,
+  roles_id SERIAL NOT NULL,
   role VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (roles_id)
 );
+
 CREATE TABLE permissions (
-  id SERIAL NOT NULL,
+  permission_id SERIAL NOT NULL,
   permission VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (permission_id)
 );
 CREATE TABLE role_permission (
   id SERIAL NOT NULL,
   role_id INT,
   permission_id INT,
-  FOREIGN KEY (role_id) REFERENCES roles(id),
-  FOREIGN KEY (permission_id) REFERENCES permissions(id),
+  FOREIGN KEY (role_id) REFERENCES roles(roles_id),
+  FOREIGN KEY (permission_id) REFERENCES permissions(permission_id),
   PRIMARY KEY (id)
 );
-CREATE TABLE specialty  (
- specialty_id SERIAL PRIMARY KEY,
-specialty  VARCHAR(255),
-    created_at TIMESTAMP,
-    is_deleted SMALLINT DEFAULT 0
-);
-CREATE TABLE info (
-  id SERIAL PRIMARY KEY,
-diagnosis VARCHAR(255),
-users INT,
-provider INT,
-appointment INT,
-    created_at TIMESTAMP,
-    is_deleted SMALLINT DEFAULT 0,
-FOREIGN KEY (appointment) REFERENCES appointment(appointment_id),
-    FOREIGN KEY (users) REFERENCES users(id),
-    FOREIGN KEY (provider) REFERENCES provider(provider_id)
-);
-CREATE TABLE notification (
-  notification_id SERIAL PRIMARY KEY,
- notification  VARCHAR(255),
-users INT,
-provider INT,
-   created_at TIMESTAMP,
-    is_deleted SMALLINT DEFAULT 0,
- FOREIGN KEY (users) REFERENCES users(id),
-    FOREIGN KEY (provider) REFERENCES provider(provider_id)
-);
-CREATE TABLE provider(
-provider_id SERIAL PRIMARY KEY,
-name VARCHAR(255) NOT NULL,
-phone INT,
-email VARCHAR(255) UNIQUE NOT NULL,
-password VARCHAR(255),
-location VARCHAR(255),
-workingHours VARCHAR(255),
-insurance VARCHAR(255),
-specialty INT,
-appointment INT,
-category INT,
-role_id INT,
- created_at TIMESTAMP,
-is_deleted SMALLINT DEFAULT 0,
- FOREIGN KEY (specialty) REFERENCES specialty(specialty_id),
-FOREIGN KEY (appointment) REFERENCES appointment(appointment_id),
-FOREIGN KEY (category) REFERENCES category(category_id),
-FOREIGN KEY (role_id) REFERENCES roles(id),
- PRIMARY KEY (id)
-);
-CREATE TABLE users (
-  id SERIAL NOT NULL,
-  firstName VARCHAR(255),
-  lastName VARCHAR(255),
-  age INT,
-phone INT,
-  email VARCHAR(255) UNIQUE,
-  password VARCHAR(255),
-  role_id INT,
-  is_deleted SMALLINT DEFAULT 0,
-  FOREIGN KEY (role_id) REFERENCES roles(id),
-  PRIMARY KEY (id)
-);
-CREATE TABLE review (
-  review_id SERIAL PRIMARY KEY,
-comments  VARCHAR(255),
-users INT,
-    created_at TIMESTAMP,
-    is_deleted SMALLINT DEFAULT 0,
-    FOREIGN KEY (users) REFERENCES users(id),
-);
-CREATE TABLE category (
- category_id SERIAL PRIMARY KEY,
-category  VARCHAR(255),
-    created_at TIMESTAMP,
-    is_deleted SMALLINT DEFAULT 0
-);
-CREATE TABLE appointment (
-  appointment_id SERIAL PRIMARY KEY,
-date DATE,
+
+CREATE TABLE history (
+ history_id SERIAL PRIMARY KEY,
 medications VARCHAR(255),
 medicalHistory VARCHAR(255),
-notes VARCHAR(255),
-users INT,
     created_at TIMESTAMP,
-    is_deleted SMALLINT DEFAULT 0,
-FOREIGN KEY (users) REFERENCES users(id)
+    is_deleted SMALLINT DEFAULT 0
 );
-CREATE TABLE provider_review(
-id SERIAL PRIMARY KEY,
-    provider_id INT,
-    review_id INT,
-  FOREIGN KEY (provider_id) REFERENCES provider(provider_id),
-    FOREIGN KEY (review_id) REFERENCES review(review_id)
+
+CREATE TABLE insurance (
+insurance_id SERIAL PRIMARY KEY,
+insuranceName VARCHAR(255),
+is_deleted SMALLINT DEFAULT 0
 );
+
+
+CREATE TABLE specialty (
+specialty_id SERIAL PRIMARY KEY,
+specialty VARCHAR(255),
+ created_at TIMESTAMP
+)
+
+CREATE TABLE users(
+users_id SERIAL PRIMARY KEY,
+firstName VARCHAR(255) NOT NULL,
+lastName VARCHAR(255),
+ age INT,
+phone INT NOT NULL,
+email VARCHAR(255) UNIQUE NOT NULL,
+password VARCHAR(255) NOT NULL,
+address VARCHAR(255),
+history INT,
+insurance INT,
+specialty INT,
+role_id INT,
+is_deleted SMALLINT DEFAULT 0,
+ FOREIGN KEY (specialty) REFERENCES specialty(specialty_id),
+  FOREIGN KEY (insurance) REFERENCES insurance(insurance_id),
+ FOREIGN KEY (history) REFERENCES history(history_id),
+FOREIGN KEY (role_id) REFERENCES roles(roles_id)
+
+);
+
+CREATE TABLE docInfo (
+docInfo_id SERIAL PRIMARY KEY,
+specialty  INT,
+provider_id INT,
+FOREIGN KEY (provider_id) REFERENCES users(users_id),
+FOREIGN KEY (specialty) REFERENCES specialty(specialty_id),
+experience VARCHAR(255),
+certificates VARCHAR(255),
+is_deleted SMALLINT DEFAULT 0
+);
+
+CREATE TABLE notes (
+ notes_id SERIAL PRIMARY KEY,
+notes VARCHAR(255),
+users_id INT,
+provider_id INT,
+FOREIGN KEY (users_id) REFERENCES users(users_id),
+FOREIGN KEY (provider_id) REFERENCES users(users_id),
+created_at TIMESTAMP,
+ is_deleted SMALLINT DEFAULT 0
+);
+
+CREATE TABLE review (
+ review_id SERIAL PRIMARY KEY,
+comment VARCHAR(255),
+users_id INT,
+provider_id INT,
+FOREIGN KEY (users_id) REFERENCES users(users_id),
+FOREIGN KEY (provider_id) REFERENCES users(users_id),
+created_at TIMESTAMP,
+ is_deleted SMALLINT DEFAULT 0
+);
+
+ CREATE TABLE schedule (
+ schedule_id SERIAL PRIMARY KEY,
+ date DATE,
+ timeFrom TIME,
+ timeTo TIME,
+provider_id INT,
+user_id INT,
+FOREIGN KEY (user_id) REFERENCES users(users_id),
+FOREIGN KEY (provider_id) REFERENCES users(users_id),
+status varchar(255) DEFAULT 'pending'
+);
+
+CREATE TABLE notification (
+notification_id SERIAL PRIMARY KEY,
+notification  VARCHAR(255),
+users_id INT,
+schedule_id INT,
+created_at TIMESTAMP,
+FOREIGN KEY (schedule_id) REFERENCES schedule(schedule_id),
+is_deleted SMALLINT DEFAULT 0,
+ FOREIGN KEY (users_id) REFERENCES users(users_id)
+);
+
+
+
+
+
 
