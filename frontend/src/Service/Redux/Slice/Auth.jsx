7022ae841af215b9944030fee113876d038bb91b
RadwanTbeileh <tbeileh.radwan@gmail.com>
2024-05-13 23:01:50 +0300

    make slice folder and auth is raedy.

---
+++ frontend/src/Service/Redux/Slice/Auth.jsx
@@ -0,0 +1,37 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+export const authSlice = createSlice({
+name:"auth",
+initialState:{
+    token : null || localStorage.getItem("token"),
+    userId : null || localStorage.getItem("userId"),
+    isLoggedIn : false
+},
+
+reducers:{
+setLogin :(state,action)=>{
+  //  console.log(action.payload);
+state.token = action.payload.token;
+state.isLoggedIn = true;
+localStorage.setItem("token",state.token)
+},
+
+setUserId :(state,action)=>{
+    state.userId = action.payload;
+localStorage.setItem("userId",state.userId)
+},
+
+setLogout :(state,action)=>{
+state.token = null;
+state.userId = null;
+state.isLoggedIn = false;
+localStorage.clear();
+    
+}
+}
+})
+
+
+export const {setLogin, setUserId,setLogout } = authSlice.actions;
+
+export default authSlice.reducer;
