fa4a2bf43db75401db5683273e669b8f50146403
FarahRefaie <farahrefaie.fr@gmail.com>
2024-05-14 21:13:19 +0300

    register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import "./App.css";
-const App = () => {
+
+import './App.css'
+
+function App() {
+
+
   return (
-   <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+
+    </>
   )
 }
 
