cb4526233ae83d53b876d9e7a5bb6ec61e3eb9f8
RadwanTbeileh <tbeileh.radwan@gmail.com>
2024-05-15 22:25:23 +0300

    schedule slice

---
+++ frontend/src/Service/Redux/Slice/Schedules.jsx
@@ -0,0 +1,31 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+export const schedulesSlice = createSlice({
+    name: "schedules",
+    initialState: {
+        schedules: [],
+    },
+    reducers: {
+      setSchedules: (state, action) => {
+        state.schedules = action.payload;
+      },
+      
+      updateSchedules: (state, action) => {
+        state.schedules = state.schedules.map((elem, i) => {
+          if (elem.schedules_id == action.payload.schedules_id) {
+            elem = action.payload.schedules;
+          }
+          return elem;
+        });
+      },
+      addSchedules: (state, action) => {
+        state.schedules = state.schedules.map((elem, i) => {
+          if (elem.schedules_id == action.payload.schedules_id) {
+            return elem.schedules.push(action.payload.schedules);
+          }
+        });
+      },
+    },
+  });
+  export const {setSchedules,updateSchedules,addSchedules} = schedulesSlice.actions
+  export default schedulesSlice.reducer;
