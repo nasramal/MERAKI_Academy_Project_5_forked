32965af8f55d65d981b7682d50bcc57d046d3736
nasramal <amalradwan.nasr@gmail.com>
2024-05-15 22:26:44 +0300

    add the notfound page

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import "./NotFound.css";
+import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="not-found">
+    <h1>404 - Page Not Found</h1>
+    <a href="/">
+      <img src="https://via.placeholder.com/300x200.png?text=Page+Not+Found" alt="medical-image" />
+    </a>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <Link to="/" className="go-back-home-button">Go Back Home</Link>
+    
+  </div>
   )
 }
 
