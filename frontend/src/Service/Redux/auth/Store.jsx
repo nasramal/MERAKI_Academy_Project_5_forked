085dbc4b5871256374f667ff9841cdc7db28d289
nasramal <amalradwan.nasr@gmail.com>
2024-05-18 19:19:03 +0300

    add history to the user page

--- frontend/src/Service/Redux/auth/Store.jsx
+++ frontend/src/Service/Redux/auth/Store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit"; 
 // import the reducer
 import authReducer from "../Slice/Auth";
-import users from "../Slice/Users";
+import usersReducer from "../Slice/Users";
 import schedules from "../Slice/Schedules";
 import appointment from "../Slice/Appointment";
 import provider from "../Slice/Provider"
@@ -12,7 +12,7 @@ export default configureStore({
   reducer: {
     // add the reducers to the store
     auth : authReducer,
-    users: users,
+    users: usersReducer,
     appointment:appointment,
     provider:provider,
     schedules:schedules
