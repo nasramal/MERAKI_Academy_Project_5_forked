a7fa1eb253ec4cf26dc3433d1d8cec8147dd2d49
nasramal <amalradwan.nasr@gmail.com>
2024-05-21 21:18:41 +0300

    edit the search fun

--- frontend/src/pages/NaveBar/SearchResult.jsx
+++ frontend/src/pages/NaveBar/SearchResult.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-// import NavBar from "../../pages/NaveBar/NavBar"
 import { useNavigate } from "react-router-dom";
 import { useDispatch,useSelector } from "react-redux";
 import axios from "axios";
 import "./SearchResult.css"
-// import Provider from "../../pages/Provider/Provider"
-// import { setProviderId } from '../../Service/Redux/Slice/ProviderId';
-// import { FcSearch } from "react-icons/fc";
+import { setProviderId } from "../../Service/Redux/Slice/ProviderId";
+
 
 const SearchResult = () => {
   const navigate = useNavigate()
@@ -24,7 +22,6 @@ const SearchResult = () => {
   const getDoctorsByName = (searchTerm) => {
     axios.get(`http://localhost:5000/users/searchbyname/?firstname=${searchTerm}`)
       .then((result) => {
-        console.log(result);
         setSearchOptions(result.data.result);
       })
       .catch((err) => {
@@ -35,31 +32,26 @@ const SearchResult = () => {
   const handleSearchChange = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
-    // Fetch data based on the current search query
     if (query) {
       getDoctorsByName(query);
     } else {
-      // Clear search options if searchQuery is empty
       setSearchOptions([]);
     }
   };
 
   const handleSearchSubmit = () => {
-    // Perform search based on searchQuery
-    console.log('Searching for:', searchQuery);
     if (searchQuery) {
       getDoctorsByName(searchQuery);
     } else {
-      // Clear search options if searchQuery is empty
       setSearchOptions([]);
     }
   };
 
   const handleOptionClick = (option) => {
 
-    
-  
-    navigate(`/Provider/${option.users_id}`)
+    dispatch(setProviderId(option))
+
+    navigate(`/Providers`)
      
     
     } 
@@ -83,7 +75,6 @@ const SearchResult = () => {
         />
        
       </div>
-      {/* Render dropdown list of options */}
       {searchOptions.length > 0 && (
         <div className="dropdown">
           <ul>
