c6adaa79fc47e732212c8342945145a91157ff58
RadwanTbeileh <154769047+RadwanTbeileh@users.noreply.github.com>
2024-05-21 22:34:55 +0300

    cansel

--- frontend/src/pages/Register/RegisterDoctor.jsx
+++ frontend/src/pages/Register/RegisterDoctor.jsx
@@ -108,13 +108,13 @@ console.log(speciality)
             ))}
           </select>
           <br />
-          {/* <input
+          <input
             type="text"
             placeholder="Address"
             value={address}
             onChange={(e) => setAddress(e.target.value)}
             required
-          /> */}
+          />
           <br />
           <input
             type="tel"
