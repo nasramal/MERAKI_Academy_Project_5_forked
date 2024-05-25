abef2dc17aac45a24faed9e81398f26085fdac75
FarahRefaie <farahrefaie.fr@gmail.com>
2024-05-25 19:30:17 +0300

    styling components

--- frontend/src/pages/ContactUs/ContactUs.jsx
+++ frontend/src/pages/ContactUs/ContactUs.jsx
@@ -1,31 +1,46 @@
-import React from 'react'
+import React from 'react';
 import "./ContactUs.css";
+import Swal from 'sweetalert2';
+
 function ContactUs() {
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Great! we will get in touch soon",
+      showConfirmButton: false,
+      timer: 2800
+    });
+  };
+
   return (
     <div className="contact-us-container">
-    <div className="contact-us-text">
-      <h2>Contact Us</h2>
-      <p>If you have any questions, please feel free to contact us using the form on the right.</p>
+      <div className="contact-us-text">
+        <h2 className='welcoming'>We will love to hear from you!</h2>
+        <p className='feed'>If you have any question, feedback, or even a suggestion, do not hesitate to contact us and fill the form ... </p>
+      </div>
+      <div className="contact-us-form">
+        <form>
+          <div className="name">
+            <label className='bigName' htmlFor="Name">Full Name:</label>
+            <input className='name' type="text" placeholder='write your full Name'  name="name" required />
+          </div>
+          <div className="form-group">
+            <label  className='bigemail' htmlFor="email">Email Address</label>
+            <input className='email' type="email" placeholder='write your email' id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label className='bigemail' htmlFor="message">Message</label>
+            <textarea id="message" placeholder='write your thoughts here!' name="message" rows="4" required></textarea>
+          </div>
+          <button className='but' type="submit" onClick={handleClick}>Send</button>
+        </form>
+      </div>
     </div>
-    <div className="contact-us-form">
-      <form>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email Address</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Message</label>
-          <textarea id="message" name="message" rows="4" required></textarea>
-        </div>
-        <button type="submit">Send</button>
-      </form>
-    </div>
-  </div>
-  )
+  );
 }
 
-export default ContactUs
+export default ContactUs;
--- frontend/src/pages/Footer/Footer.jsx
+++ frontend/src/pages/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { FaInstagram, FaFacebookF, FaTwitter } from "react-icons/fa";
 import "./Footer.css";
+
 export default function Footer() {
   return (
     <footer className="footer-container">
@@ -19,7 +21,7 @@ export default function Footer() {
           <li>Redux</li>
           <li>Css</li>
           <li>HTML</li>
-          <li>PostgrSQL</li>
+          <li>PostgreSQL</li>
         </ul>
       </div>
       <div className="footer-section join-us">
@@ -33,11 +35,31 @@ export default function Footer() {
           </li>
         </ul>
       </div>
+      <div className="footer-section social-media">
+        <h3>Follow Us</h3>
+        <ul>
+          <li>
+            <a href="https://www.instagram.com/your_instagram_handle">
+              <FaInstagram /> <label className="inf" htmlFor="firstName"> /dr.code</label>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/your_facebook_page">
+              <FaFacebookF /><label className="inf" htmlFor="firstName"> /dr.code</label>
+            </a>
+          </li>
+          <li>
+            <a  href="https://twitter.com/your_twitter_handle">
+              <FaTwitter /><label className="inf" htmlFor="firstName"> /dr.code</label>
+            </a>
+          </li>
+        </ul>
+      </div>
       <div className="footer-section contact-us">
         <h3>Contact Us</h3>
         <ul>
           <li>Amman, Jordan</li>
-          <li>
+          <li> 
             <a href="mailto:tbeileh.radwan@gmail.com">
               tbeileh.radwan@gmail.com
             </a>
